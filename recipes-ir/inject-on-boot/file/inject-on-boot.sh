7fca92bead327144192221461c2fd41d819927f2
Ben Gardiner <ben.l.gardiner@gmail.com>
2015-12-09 12:18:39 -0500

    Update inject-on-boot.sh

--- recipes-ir/inject-on-boot/file/inject-on-boot.sh
+++ recipes-ir/inject-on-boot/file/inject-on-boot.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
+udhcpc -i eth0 &
+export DISPLAY=:0
+/usr/bin/infotainment_demo &
+
+next_evdev="$(( $(ls /dev/input/event* | wc -l) + 1 ))"
 timerinputme &
+evdev-usb-bridge /dev/input/event${next_evdev} &
+
 cd /usr/bindings/USBProxy/src/tools/ && python3 buttonsme &
 cd /usr/bindings/USBProxy/src/bindings/python && ./usbproxy-fd-keyboard.py
+
