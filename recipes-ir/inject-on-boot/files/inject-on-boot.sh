cc709ef0c2857911b0f42a714cc30dc75ace2a01
Ben Gardiner <ben.gardiner@irdeto.com>
2015-12-09 14:15:13 -0800

    update rootfs init script with locations found in core-image-x11

--- recipes-ir/inject-on-boot/files/inject-on-boot.sh
+++ recipes-ir/inject-on-boot/files/inject-on-boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 udhcpc -i eth0 &
+
 export DISPLAY=:0
 /usr/bin/infotainment_demo &
 
@@ -8,6 +9,6 @@ next_evdev="$(( $(ls /dev/input/event* | wc -l) + 1 ))"
 timerinputme &
 evdev-usb-bridge /dev/input/event${next_evdev} &
 
-cd /usr/bindings/USBProxy/src/tools/ && python3 buttonsme &
-cd /usr/bindings/USBProxy/src/bindings/python && ./usbproxy-fd-keyboard.py
+python3 $(which buttonsme) &
+cd /usr/bindings/python/USBProxy && ./usbproxy-fd-keyboard.py &
 
