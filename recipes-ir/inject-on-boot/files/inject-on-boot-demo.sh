d090f0300fe6301cc3f136de33d4da5a51a65b31
Hammad Ahmed <hammad.ahmed@irdeto.com>
2015-12-15 21:37:48 -0500

    - Updating inject-on-boot-demo.sh to include venturo

--- recipes-ir/inject-on-boot/files/inject-on-boot-demo.sh
+++ recipes-ir/inject-on-boot/files/inject-on-boot-demo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ -e /dev/ttyACM0 ]
-  then
-    python3 -B $(which buttonsme) &
-    sleep 15
-    echo "Turning on box lights for Demo"
-    echo -en '\xAD' > /dev/ttyACM0
-    echo -en '\x2D' > /dev/ttyACM0
+if [ -e /dev/ttyACM0 ]; then
+  python3 -B $(which buttonsme) &
+  sleep 15
+  echo "Turning on box lights for Demo"
+  echo -en '\xAD' > /dev/ttyACM0
+  echo -en '\x2D' > /dev/ttyACM0
+else
+  $(which venturo) &
 fi
 
