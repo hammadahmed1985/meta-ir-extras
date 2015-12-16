7ba379ea6fb5c540c44a1efa79dadfe2225bf6d3
Hammad Ahmed <hammad.ahmed@irdeto.com>
2015-12-15 21:22:10 -0500

    - Updating inject-on-boot-demo.sh to start buttomsme only if Arduino is connected - Adding code and delay to light up the demo box leds and buttons

--- recipes-ir/inject-on-boot/files/inject-on-boot-demo.sh
+++ recipes-ir/inject-on-boot/files/inject-on-boot-demo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-kill -9 $(pidof udhcpc)
-
-python3 -B $(which buttonsme) &
+if [ -e /dev/ttyACM0 ]
+  then
+    python3 -B $(which buttonsme) &
+    sleep 15
+    echo "Turning on box lights for Demo"
+    echo -en '\xAD' > /dev/ttyACM0
+    echo -en '\x2D' > /dev/ttyACM0
+fi
 
